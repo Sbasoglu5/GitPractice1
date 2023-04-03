c4c9254da055b7f670baac2349304a0f17066982
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-04-03 19:56:40 -0400

    Olha's features

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         Class1 class1=new Class1();
         class1.name="Hamid";
         class1.age="24";
+
+        OlhaFeatures add=new OlhaFeatures();
+        add.name="Olha";
+        add.age=22;
     }
 }
---
+++ src/OlhaFeatures.java
@@ -0,0 +1,4 @@
+public class OlhaFeatures {
+    String name;
+    int age;
+}
