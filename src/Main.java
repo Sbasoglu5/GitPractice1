db886af4945c78984454e7c5395d8c9cf7b697c6
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-04-03 20:05:37 -0400

    Savo's features

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         Class1 class1=new Class1();
         class1.name="Hamid";
         class1.age="24";
+
+        SavoFeatures add1=new SavoFeatures();
+        add1.name="Savo";
+        add1.age=25;
+
     }
 }
